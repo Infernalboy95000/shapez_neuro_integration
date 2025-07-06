2383bae31a3f4de5a7421154fe3bbdf966f338e2
Infernal-Ekoro <39791183+Infernalboy95000@users.noreply.github.com>
2025-07-06 13:30:03 +0200

    Basic HUB descriptor
    + Describes position
    + Describes position and direction where it accepts items

--- js/actions/helpers/buildingDescriptor.js
+++ js/actions/helpers/buildingDescriptor.js
@@ -2,6 +2,7 @@ import { MapChunkView } from "shapez/game/map_chunk_view";
 import { EntityComponentStorage } from "shapez/game/entity_components";
 import { RotationCodes } from "./rotationCodes";
 import { Entity } from "shapez/game/entity";
+import { Vector } from "shapez/core/vector";
 
 export class BuildingDescriptor {
 	/** @type {MapChunkView} */ #chunk
@@ -37,6 +38,8 @@ export class BuildingDescriptor {
 				return this.#describeBelt(id, building);
 			case "miner":
 				return this.#describeMiner(id, building);
+			case "hub":
+				return this.#describeHub(id, building);
 			default:
 				return {msg:"Unknown building", describedIDs:new Array()};
 		}
@@ -110,13 +113,55 @@ export class BuildingDescriptor {
 	 * @returns {{msg:string, describedIDs:Array<number>}}
 	 * */
 	static #describeMiner(id, miner) {
-		let log = {msg:"", describedIDs:new Array()};
+		const log = {msg:"", describedIDs:new Array()};
 		const origin = miner.StaticMapEntity.origin;
 		const currentDirection = miner.StaticMapEntity.originalRotation;
 		const rotName = RotationCodes.getRotationName(currentDirection);
 		log.msg = `Found miner at x: ${origin.x}, y: ${origin.y} ` +
 		`facing ${rotName}`;
 		log.describedIDs.push(id);
+
+		return log;
+	}
+
+	/**
+	 * @param {Number} id
+	 * @param {EntityComponentStorage} hub
+	 * @returns {{msg:string, describedIDs:Array<number>}}
+	 * */
+	static #describeHub(id, hub) {
+		/** @type {Map<string, Array<Vector>>} */
+		const slotsByDir = new Map();
+		const log = {msg:"", describedIDs:new Array()};
+		const origin = hub.StaticMapEntity.origin;
+		const slots = hub.ItemAcceptor.slots;
+		log.describedIDs.push(id);
+
+		for (let i = 0; i < slots.length; i++) {
+			const pos = slots[i].pos.add(origin);
+			let accceptors;
+			if (slotsByDir.has(slots[i].direction)) {
+				accceptors = slotsByDir.get(slots[i].direction);
+				accceptors.push(pos);
+			}
+			else {
+				accceptors = new Array(pos);
+			}
+			slotsByDir.set(slots[i].direction, accceptors);
+		}
+
+		log.msg = `Found hub at x: ${origin.x}, y: ${origin.y}.`;
+		if (slots.length > 0) {
+			log.msg += ` Accepts items:`
+		}
+
+		slotsByDir.forEach((slots, direction) => {
+			log.msg += `\nfrom ${direction}:`
+
+			for (let i = 0; i < slots.length; i++) {
+				log.msg += ` x: ${slots[i].x}, y: ${slots[i].y}.`;
+			}
+		});
 
 		return log;
 	}
