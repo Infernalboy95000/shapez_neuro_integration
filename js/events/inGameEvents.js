1cc235d1059e7d5fbca54e81def4e2e5222ffdc8
Infernal-Ekoro <39791183+Infernalboy95000@users.noreply.github.com>
2025-09-09 23:36:54 +0200

    SDK detects player movement
    - Will activate movement actions after certain time of movement
    - Will wait 5 seconds after player movement is detected
    - Will act instantly after moved by the SDK or by markers

--- js/events/inGameEvents.js
+++ js/events/inGameEvents.js
@@ -5,10 +5,21 @@ import { HUDPinnedShapes } from "shapez/game/hud/parts/pinned_shapes";
 import { OverlayEvents } from "./overlayEvents";
 import { HUDWaypoints } from "shapez/game/hud/parts/waypoints";
 import { MarkersDescriptor } from "../actions/descriptors/pins/markersDescriptor";
+import { Vector } from "shapez/core/vector";
+import { RandomUtils } from "../custom/randomUtils";
+
+const TIME_WAIT_FOR_PLAYER = 5 * 1000;
+const ZOOM_TOLERANCE = 1;
+const MOVE_TOLERANCE = 3;
 
 export class InGameEvents {
 	/** @type {import("shapez/game/root").GameRoot} */ #root;
+	/** @type {number} */ #waitTime = 0;
 	/** @type {boolean} */ #moving;
+	/** @type {boolean} */ #movingBySdk;
+	/** @type {boolean} */ #movingByPlayer;
+	/** @type {number} */ #lastZoomLevel;
+	/** @type {Vector} */ #lastPosition;
 
 	/** @param {import("../main").NeuroIntegration} mod */
 	constructor(mod) {
@@ -16,7 +27,7 @@ export class InGameEvents {
 
 		mod.modInterface.runAfterMethod(GameCore, "tick",
 			function(deltaMs) {
-				thisClass.#checkCameraMovement();
+				thisClass.#checkCameraMovement(deltaMs);
 				return true;
 			}
 		);
@@ -49,34 +60,77 @@ export class InGameEvents {
 	/** @param {import("shapez/game/root").GameRoot} root */
 	updateRoot(root) {
 		this.#root = root;
+		this.#lastPosition = this.#root.camera.center;
+		this.#lastZoomLevel = this.#root.camera.zoomLevel;
 		root.signals.editModeChanged.add(this.#layersSwitched, this);
 		root.signals.upgradePurchased.add(this.#refreshShop, this);
 		root.hud.signals.shapePinRequested.add(this.#refreshPins, this);
 		OverlayEvents.OVERLAYS_CLOSED.add("event_overs_closed", () => { this.#onDialogClosed(); });
 	}
 
-	//TODO This doesn't work when the player moves the camera slightly with mouse movement or keyboard
-	#checkCameraMovement() {
+	/** @param {number} deltaMs */
+	#checkCameraMovement(deltaMs) {
 		if (!this.#root || !this.#root.camera) {
 			return;
 		}
 
-		if (this.#moving) {
-			if (!this.#root.camera.viewportWillChange()) {
-				this.#moving = false;
-				ActionsCollection.activateActions([
-					"build", "delete", "massDelete", "scan", "camera", "marker"
-				]);
-			}
-		}
-		else {
-			if (this.#root.camera.viewportWillChange()) {
+		if (!this.#moving) {
+			if (this.#hasMovedThisFrame()) {
 				this.#moving = true;
 				ActionsCollection.deactivateActions([
 					"build", "delete", "massDelete", "scan", "camera", "marker"
 				]);
 			}
 		}
+		else if (this.#hasMovedThisFrame()) {
+			this.#waitTime = 0;
+		}
+		else {
+			this.#waitTime += deltaMs;
+			if (this.#movingByPlayer) {
+				if (this.#waitTime >= TIME_WAIT_FOR_PLAYER) {
+					this.#waitTime = 0;
+					this.#moving = false;
+					this.#movingByPlayer = false;
+					ActionsCollection.activateActions([
+						"build", "delete", "massDelete", "scan", "camera", "marker"
+					]);
+				}
+			}
+			else if (this.#movingBySdk) {
+				this.#waitTime = 0;
+				this.#moving = false;
+				this.#movingBySdk = false;
+				ActionsCollection.activateActions([
+					"build", "delete", "massDelete", "scan", "camera", "marker"
+				]);
+			}
+		}
+
+		if (!RandomUtils.vectorsEqualAprox(this.#lastPosition, this.#root.camera.center, MOVE_TOLERANCE))
+			this.#lastPosition = this.#root.camera.center;
+
+		if (!RandomUtils.numbersEqualAprox(this.#root.camera.zoomLevel, this.#lastZoomLevel, ZOOM_TOLERANCE))
+			this.#lastZoomLevel = this.#root.camera.zoomLevel;
+	}
+
+	/** @returns {boolean} */
+	#hasMovedThisFrame() {
+		if (this.#root.camera.desiredCenter !== null || this.#root.camera.desiredZoom !== null) {
+			this.#movingBySdk = true;
+			return true;
+		}
+
+		if (!RandomUtils.vectorsEqualAprox(this.#lastPosition, this.#root.camera.center, MOVE_TOLERANCE)) {
+			this.#movingByPlayer = true;
+			return true;
+		}
+
+		if (!RandomUtils.numbersEqualAprox(this.#root.camera.zoomLevel, this.#lastZoomLevel, ZOOM_TOLERANCE)) {
+			this.#movingByPlayer = true;
+			return true;
+		}
+		return false;
 	}
 
 	/** @param {Layer} layer */
